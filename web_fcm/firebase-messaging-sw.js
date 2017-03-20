15d7fe9af376f49ee639c59a5b05141c14f052d7
dojo8888 <dojo8888@gmail.com>
2017-03-20 22:28:09 +0900

    service worker

--- web_fcm/firebase-messaging-sw.js
+++ web_fcm/firebase-messaging-sw.js
@@ -1,8 +1,6 @@
 importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-messaging.js');
 
-
-// Initialize Firebase
 var config = {
     apiKey: "AIzaSyB9zcIc1TpWX76Y9idkUgzPsDQrfCrW-T0",
     authDomain: "tank-6d5d3.firebaseapp.com",
