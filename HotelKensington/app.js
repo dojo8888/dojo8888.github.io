f2a42e13efdcd7d488cb35926c146f8cd9826c85
dojo8888 <dojo8888@gmail.com>
2019-11-22 13:58:06 +0900

    hotel kensington

---
+++ HotelKensington/app.js
@@ -0,0 +1,100 @@
+var deviceType = (function(){
+    var device= "";
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    if( userAgent.match( /iPad/i ) || userAgent.match( /iPhone/i ) || userAgent.match( /iPod/i ) ){
+        device= 'ios';
+    }else if( userAgent.match( /Android/i ) ){
+        device= 'android';
+    }else{
+        device= 'unknown'; 
+    }   
+    return device;
+})();
+
+isAndroid = (function(){
+  return deviceType=="android" && typeof Android != "undefined";
+})();
+
+isIOS = (function(){
+  return deviceType=="ios" && typeof IOS != "undefined";
+})();
+
+var postMessage = function(bridgeName, message) {
+    if (isAndroid){
+        Android[bridgeName](JSON.stringify(message));
+    } else if (isIOS) {
+        IOS[bridgeName].postMessage(message);
+    } else {
+        console.log(bridgeName, message);
+    }
+}
+
+var simpleAlert = (...options) => {
+    alert(options.join(" + "));
+}
+
+var appCallback = {
+    "getJson" : simpleAlert, 
+    "getPushToken":simpleAlert,
+    "getLocation":simpleAlert,
+    "getVersion":simpleAlert
+}
+
+var appRequest = {
+    "setJson" : () => {
+        let message = {
+            "key" : "test",
+            "data" : JSON.stringify(
+              {
+                "keyA" : "aaaa",
+                "keyB" : "bbbb"
+              }
+            )
+        }
+        postMessage("setJson", message);
+    },
+    "getJson": () => {
+        let message = {
+            "key" : "test",
+            "callback" : "appCallback.getJson"
+        }
+        postMessage("getJson",message);
+    },
+    "getPushToken": () => {
+        let message = {
+            "callback" : "appCallback.getPushToken"
+        }
+        postMessage("getPushToken",message);
+    },
+    "getLocation": () => {
+        let message = {
+            "callback" : "appCallback.getLocation"
+        }
+        postMessage("getLocation",message);
+    },
+    "getVersion": () => {
+        let message = {
+            "callback" : "appCallback.getVersion"
+        }
+        postMessage("getVersion",message);
+    },
+    "openUrlEx": () => {
+        let message = {
+            "url" : "http://m.naver.com"
+        }
+        postMessage("openUrlEx",message);
+    },
+    "openUrlPop" : () => {
+        let message = {
+            "url" : "http://m.naver.com",
+            "title" : "네이버"
+        }
+        postMessage("openUrlPop",message);
+    },
+    "fileDownload" : () => {
+        let message = {
+            "url" : "https://ani24like.com/img/ani/3566.jpg"
+          }
+        postMessage("fileDownload",message);
+    },
+}
